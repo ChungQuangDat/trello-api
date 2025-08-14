abfe37902ec4c418f0c3d0b95a9ff76eb943ba43
Chung Quang Dat <chungdat3918@gmail.com>
2025-08-14 21:25:31 +0700

    Init code

---
+++ src/server.js
@@ -0,0 +1,16 @@
+// const express = require('express')
+import express from 'express'
+
+const app = express()
+
+const hostname = 'localhost'
+const port = 8017
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hello World</h1>')
+})
+
+app.listen(port, hostname, () => {
+  console.log(`hello dat, I'm running server at http://${hostname}:${port}/`)
+
+})
